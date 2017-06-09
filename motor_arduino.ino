415f228a84d2940002b9c2b65c026e32730721c9
Alexandru Niculae <alexandru.niculae@microchip.com>
2017-06-09 12:33:35 +0300

    fixed mottors cross wired

--- motor_arduino.ino
+++ motor_arduino.ino
@@ -1,8 +1,8 @@
 #include <SoftwareSerial.h>
 SoftwareSerial hc05(8, 10); // RX, TX
 int power_1 = 9;
-int power_2 = 11;
-int power_3 = 13;
+int power_2 = 13;
+int power_3 = 11;
 
 void motor1_ahead() {
   digitalWrite(2, HIGH);
@@ -41,7 +41,7 @@ void setup() {
   pinMode(5, OUTPUT);
   pinMode(6, OUTPUT);
   pinMode(7, OUTPUT);
-  
+
   pinMode(power_1, OUTPUT);
   pinMode(power_2, OUTPUT);
   pinMode(power_3, OUTPUT);
@@ -50,16 +50,16 @@ void setup() {
   motor2_ahead();
   motor3_ahead();
 
-//  for(int p = 0; p <= 250; p+=10) {
-//    analogWrite(power_1, p);
-//  }
+  //  for(int p = 0; p <= 250; p+=10) {
+  //    analogWrite(power_1, p);
+  //  }
 
-  analogWrite(power_1, 124);
-//  delay(1000);
-  analogWrite(power_2, 124);
-//  delay(1000);
-  analogWrite(power_3, 124);
-//  delay(1000);
+  analogWrite(power_1, 0);
+  //  delay(1000);
+  analogWrite(power_2, 0);
+  //  delay(1000);
+  analogWrite(power_3, 0);
+  //  delay(1000);
   Serial.begin(9600);
   hc05.begin(9600);
 }
@@ -67,61 +67,57 @@ char command[20];
 int p = 0;
 
 void exec(char *command) {
-  char buf[3];
+  char buf[4];
   int p;
-  Serial.println(command);    
-  // Read PWM value for motor 1      
-  memcpy(buf, &command[0], 4*sizeof(char));
+
+  // Read PWM value for motor 1
+  memcpy(buf, &command[0], 4 * sizeof(char));
   p = atoi(buf);
-  Serial.println(buf); 
-  if(p > 0) {
+  if (p > 0) {
     motor1_ahead();
     analogWrite(power_1, p);
   }
   else {
     motor1_back();
-    analogWrite(power_1, p*-1);
+    analogWrite(power_1, p * -1);
   }
 
   // Read PWM value for motor 2
-  memcpy(buf, &command[5], 4*sizeof(char));
+  memcpy(buf, &command[5], 4 * sizeof(char));
   p = atoi(buf);
-  Serial.println(buf); 
-  if(p > 0) {
+  if (p > 0) {
     motor2_ahead();
     analogWrite(power_2, p);
   }
   else {
     motor2_back();
-    analogWrite(power_2, p*-1);
+    analogWrite(power_2, p * -1);
   }
 
   // Read PWM value for motor 3
-  memcpy(buf, &command[10], 4*sizeof(char));
+  memcpy(buf, &command[10], 4 * sizeof(char));
   p = atoi(buf);
-  Serial.println(buf); 
-  if(p > 0) {
+  if (p > 0) {
     motor3_ahead();
     analogWrite(power_3, p);
   }
   else {
     motor3_back();
-    analogWrite(power_3, p*-1);
+    analogWrite(power_3, p * -1);
   }
-  
 }
 
 void loop() {
 
- if(hc05.available() > 0){ // Checks whether data is comming from the serial port
+  if (hc05.available() > 0) { // Checks whether data is comming from the serial port
     char c = hc05.read();
-    if( c != '\n' && c != 0 ){
+    if ( c != '\n' && c != 0 ) {
       command[p++] = c;
-    } else if(c == '\n') {
+    } else if (c == '\n') {
       command[p] = '\0';
       exec(command);
       p = 0;
     }
- }
+  }
 
 }
